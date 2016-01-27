3129e2059f9774ff0e05e84056212a3523f91035
Matthew Gerig <mattgerig@gmail.com>
2016-01-27 10:18:13 +0000

    FPS counter, FIX

--- Galway_Project/Galway_Project.cpp
+++ Galway_Project/Galway_Project.cpp
@@ -4,6 +4,86 @@
 #include "stdafx.h" 
 #include "MyThreadPool.h"
 
+#define FRAME_VALUES 10
+
+// An array to store frame times:
+Uint32 frametimes[FRAME_VALUES];
+
+// Last calculated SDL_GetTicks
+Uint32 frametimelast;
+
+// total frames rendered
+Uint32 framecount;
+
+// the value you want
+float framespersecond;
+
+// This function gets called once on startup.
+void fpsinit() {
+
+	// Set all frame times to 0ms.
+	memset(frametimes, 0, sizeof(frametimes));
+	framecount = 0;
+	framespersecond = 0;
+	frametimelast = SDL_GetTicks();
+
+}
+
+void fpsthink() {
+
+	Uint32 frametimesindex;
+	Uint32 getticks;
+	Uint32 count;
+	Uint32 i;
+
+	// frametimesindex is the position in the array. It ranges from 0 to FRAME_VALUES.
+	// This value rotates back to 0 after it hits FRAME_VALUES.
+	frametimesindex = framecount % FRAME_VALUES;
+
+	// store the current time
+	getticks = SDL_GetTicks();
+
+	// save the frame time value
+	frametimes[frametimesindex] = getticks - frametimelast;
+
+	// save the last frame time for the next fpsthink
+	frametimelast = getticks;
+
+	// increment the frame count
+	framecount++;
+
+	// Work out the current framerate
+
+	// The code below could be moved into another function if you don't need the value every frame.
+
+	// I've included a test to see if the whole array has been written to or not. This will stop
+	// strange values on the first few (FRAME_VALUES) frames.
+	if (framecount < FRAME_VALUES) {
+
+		count = framecount;
+
+	}
+	else {
+
+		count = FRAME_VALUES;
+
+	}
+
+	// add up all the values and divide to get the average frame time.
+	framespersecond = 0;
+	for (i = 0; i < count; i++) {
+
+		framespersecond += frametimes[i];
+
+	}
+
+	framespersecond /= count;
+
+	// now to make it an actual frames per second value...
+	framespersecond = 1000.f / framespersecond;
+
+}
+
 void CreateGround(b2World& World, float X, float Y)
 {
 	b2BodyDef BodyDef;
@@ -25,15 +105,7 @@ int main(int argc, char *argv[])
 	semGlobal = SDL_CreateSemaphore(4);
 
 	ThreadPool::GetInstance();
-
-	//fps counter code
-	//Set text color as black 
-	SDL_Color textColor = { 0, 0, 0, 255 }; //The frames per second timer 
-	LTimer fpsTimer; //In memory text stream 
-	std::stringstream timeText;
-	//Start counting frames per second 
-	int countedFrames = 0; 
-	fpsTimer.start();
+	fpsinit();
 
 	b2Vec2 Gravity(0.f, 9.8f);
 	b2World world(Gravity);
@@ -41,10 +113,7 @@ int main(int argc, char *argv[])
 	LevelManager lvlMngr(&world);
 
 	ContactListener contact = ContactListener(&lvlMngr);
-	world.SetContactListener(&contact);
-
-	Uint32 lastFrameTime = 0;
-	Uint32 frameDelay = 0;
+	world.SetContactListener(&contact); 
 
 	SDL_Event e;
 	bool quit = false;
@@ -59,18 +128,8 @@ int main(int argc, char *argv[])
 	{
 		// Update loop
 		// gets the deltaTime between frames, pass 'frameDelay' to updates		
-		Uint32 currentFrameTime = SDL_GetTicks();
-		if (currentFrameTime > lastFrameTime)
-		{
-			frameDelay = currentFrameTime - lastFrameTime;
-			lastFrameTime = currentFrameTime;
-		}
-		
-		//Calculate and correct fps 
-		float avgFPS = countedFrames / ( fpsTimer.getTicks() / 1000.f ); 
-		if( avgFPS > 2000000 ) { avgFPS = 0; }
-
-		//std::cout << "FPS is: " << avgFPS << std::endl;
+		fpsthink();
+		printf("%f\n", framespersecond);  
 
 		Render::GetInstance()->Update();
 		
@@ -109,9 +168,7 @@ int main(int argc, char *argv[])
 			}
 		}//End Poll Events
 
-		InputManager::GetInstance()->UpdateKeyboardState();
-
-		countedFrames++;
+		InputManager::GetInstance()->UpdateKeyboardState(); 
 	}//End Game loop
 	SDL_Quit();
     return 0;
